95267301a51d9d99ef95bd384f4cd1e80ac2b412
deppravacion <deppravacion@gmail.com>
2022-11-22 14:05:01 -0700

    excercise complete comp properties

--- src/App.js
+++ src/App.js
@@ -3,14 +3,37 @@ import logo from './logo.svg';
 import MyClassComp from './MyClassComp';
 import ExBuildingCompLayout from './ExBuildingCompLayout';
 import ExMappingFunction from './ExMappingFunction';
+import ExCompProperties from './ExCompProperties';
 
 function App() {
+
+  const data = {
+    image: "https://i.pinimg.com/originals/57/7e/41/577e41cc9210ed46a578fce923a5f51c.jpg",
+    cardTitle: "Harry Mack",
+    cardDescription: "Multi-talented artist and rapper Harry Mack is best known for his unique, jaw-dropping visual freestyle rapping. He first came to fame when his Venice Beach Freestyle went viral, which propelled him to opportunities alongside the likes of Kendrick Lamar, Joey Bada$$, Ellen DeGeneres, Complex Music and RedBull Music.",
+    button: {
+      url: "https://www.harrymackofficial.com/",
+      label: "Visit Website"
+    }
+  };
+
+
+
+
   return (
     <div className="App">
       <header className="App-header">
         {/* <MyClassComp propName="thisProp is propName" anotherProp="this is anotherProp sukkaFish"/> */}
-        <ExBuildingCompLayout />
+        
+        {/* <ExBuildingCompLayout /> */}
         {/* <ExMappingFunction /> */}
+        {/* <ExCompProperties 
+        title="Harry Mack"
+        image="https://i.pinimg.com/originals/57/7e/41/577e41cc9210ed46a578fce923a5f51c.jpg"
+        description="lti-talented artist and rapper Harry Mack "
+        /> */}
+    
+        <ExCompProperties info={data} />
       </header>
     </div>
   );
---
+++ src/ExCompProperties.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+class ExCompProperties extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+
+    
+    const {info: { image, cardTitle: title, cardDescription: description, button:{url, label}}} = this.props;
+
+    return (
+      <div>
+        <div
+          style={{
+            width: "18rem",
+            border: "1px solid rgba(0,0,0,0.125)",
+            margin: "1rem",
+            padding: "1rem",
+          }}
+        >
+          <img
+            
+            style={{ width: "100%", display: "block" }}
+            src={image}
+          />
+          <div>
+            <h2>{title}</h2>
+            <p>{description}</p>
+            <a href={url}>{label}</a>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ExCompProperties;
